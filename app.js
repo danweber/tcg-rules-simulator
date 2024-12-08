4fafd731046dc61093eb1350dd8f84c3bf90ec7e
Daniel <daniel@example.com>
2024-12-08 01:23:56 -0500

    ejs error fixed

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ if (true) {
     let index = Math.floor(Math.random() * words.length);
     let word = words[index]
     const url = req.protocol + '://' + req.get('host') + req.originalUrl;
-    res.render('index', { title: 'Rule Simulator v0.9.4.17.1', word: word, text: url, test: env_test});
+    res.render('index', { title: 'Rule Simulator v0.9.4.17.2', word: word, text: url, test: env_test});
   });
 
 
